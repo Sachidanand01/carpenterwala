540afded7f33bdc2d571a7abc5baf72bc822e8fd
sachidanand01 <sachidanand21@rediffmail.com>
2026-07-01 11:01:28 +0530

    Security: Missing Secure Referrer-Policy Header Fixes

--- next.config.mjs
+++ next.config.mjs
@@ -49,6 +49,10 @@ const nextConfig = {
             key: 'X-Frame-Options',
             value: 'SAMEORIGIN',
           },
+          {
+            key: 'Referrer-Policy',
+            value: 'strict-origin-when-cross-origin',
+          },
         ],
       },
     ];
