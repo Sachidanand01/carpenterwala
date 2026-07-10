c4e5125b90962bddc6ae0c6538181f10aad41985
sachidanand01 <sachidanand21@rediffmail.com>
2026-07-10 21:16:28 +0530

    addition of google tag manager

--- app/layout.js
+++ app/layout.js
@@ -111,6 +111,17 @@ export default function RootLayout({ children }) {
         </noscript>
         {/* End Meta Pixel Code */}
 
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5FQ8SPC"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+
         {/* Google AdSense Script */}
         <Script
           id="adsense-global"
@@ -132,6 +143,17 @@ export default function RootLayout({ children }) {
             gtag('js', new Date());
 
             gtag('config', 'G-RNKCPYB155');
+          `}
+        </Script>
+
+        {/* Google Tag Manager */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-5FQ8SPC');
           `}
         </Script>
 
